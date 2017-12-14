45282143f9bca29b594defe81e869a1e39c6fc5c
Taylor Lassiter <lassit09@gmail.com>
2017-12-14 02:58:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var height = "74"
 
 if (name === "Susan") {
   console.log("The naem is correct");
+} else {
+  console.log("Expected the name to be Susan");
 }
 // Don't worry about this
 module.exports = { name, height
